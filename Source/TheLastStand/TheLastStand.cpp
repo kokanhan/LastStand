8e853e6c04dc4747837406f01b5e4e22ee013e86
kokanhan <thehanjiang@gmail.com>
2024-05-25 12:35:25 +0800

    Initial commit

---
+++ Source/TheLastStand/TheLastStand.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TheLastStand.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheLastStand, "TheLastStand" );
+
+DEFINE_LOG_CATEGORY(LogTheLastStand)
+ 
